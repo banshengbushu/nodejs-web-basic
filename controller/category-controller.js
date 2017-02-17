6f12b4facdaed76ec0385d7f9683a77ee139d59b
banshengbushu <wangdanna1995@outlook.com>
2017-02-17 22:10:49 +0800

    Refactor category delete

--- controller/category-controller.js
+++ controller/category-controller.js
@@ -1,5 +1,7 @@
 const Categories = require('../model/category');
+const Item = require('../model/item');
 const httpCode = require('../config/httpCode');
+const async = require('async');
 
 class CategoryController {
   getAll(req, res, next) {
@@ -35,20 +37,38 @@ class CategoryController {
     })
   }
 
-
   delete(req, res, next) {
     const _id = req.params.categoryId;
 
-    Categories.findByIdAndRemove(_id, (err, category)=> {
+    async.waterfall([
+      (done)=> {
+        Item.findById(_id, done);
+      },
+      (docs, done)=> {
+        if (docs) {
+          done(true, null);
+        } else {
+          Categories.findByIdAndRemove(_id, (err, doc)=> {
+            if (!doc) {
+              done(false, null);
+            }
+            done(err, doc);
+          });
+        }
+      }
+    ], (err)=> {
+      if (err === true) {
+        return res.sendStatus(httpCode.BAD_REQUEST);
+      }
+      if (err === false) {
+        return res.sendStatus(httpCode.NOT_FOUND);
+      }
       if (err) {
         return next(err);
       }
-      if (!category) {
-        return res.sendStatus(httpCode.NOT_FOUND);
-      }
       return res.sendStatus(httpCode.NO_CONTENT);
-    })
 
+    });
   }
 
   update(req, res, next) {
